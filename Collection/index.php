6aead12acdd1f9965bc16a6a377bf6623c74a17d
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-08-20 17:14:00 +0700

    update the index view

---
+++ Collection/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// create function collection
+// function collection(){
+//     $data = ['taylor', 'abigail', null];
+//     return collect($data)
+//     ->map(function($name){
+//         return strtoupper($name);
+//     })->reject(function($name){
+//         return empty($name);
+//     });
+// }
+
